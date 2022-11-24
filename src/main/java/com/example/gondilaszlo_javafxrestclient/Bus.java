4c66efa2c0867bc4d363854f8e25f54387c10ce1
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-11-24 09:04:17 +0100

    Edit Bus.java

--- src/main/java/com/example/gondilaszlo_javafxrestclient/Bus.java
+++ src/main/java/com/example/gondilaszlo_javafxrestclient/Bus.java
@@ -1,9 +1,15 @@
 package com.example.gondilaszlo_javafxrestclient;
 
+import com.google.gson.annotations.Expose;
+
 public class Bus {
+    @Expose
     private int ID;
+    @Expose
     private String busID;
+    @Expose
     private int delay;
+    @Expose
     private String destination;
 
     public Bus(int ID, String busID, int delay, String destination) {
@@ -17,15 +23,31 @@ public class Bus {
         return ID;
     }
 
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
     public String getBusID() {
         return busID;
+    }
+
+    public void setBusID(String busID) {
+        this.busID = busID;
     }
 
     public int getDelay() {
         return delay;
     }
 
+    public void setDelay(int delay) {
+        this.delay = delay;
+    }
+
     public String getDestination() {
         return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.gondilaszlo_javafxrestclient {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
 
     opens com.example.gondilaszlo_javafxrestclient to javafx.fxml;
